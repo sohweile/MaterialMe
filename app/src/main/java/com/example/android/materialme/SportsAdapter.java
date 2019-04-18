26858884702017ee3504cb5b5c9c571e42b0fe0e
sohweile <sohweile@hotmail.com>
2019-04-18 16:48:41 +0800

    Initial commit

---
+++ app/src/main/java/com/example/android/materialme/SportsAdapter.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright (C) 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.materialme;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+
+class SportsAdapter extends RecyclerView.Adapter<SportsAdapter.ViewHolder>  {
+
+    private ArrayList<Sport> mSportsData;
+    private Context mContext;
+
+    SportsAdapter(Context context, ArrayList<Sport> sportsData) {
+        this.mSportsData = sportsData;
+        this.mContext = context;
+    }
+
+
+    @Override
+    public SportsAdapter.ViewHolder onCreateViewHolder(
+            ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(mContext)
+                .inflate(R.layout.list_item, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(SportsAdapter.ViewHolder holder,
+                                 int position) {
+        Sport currentSport = mSportsData.get(position);
+
+        holder.bindTo(currentSport);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mSportsData.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder
+            implements View.OnClickListener{
+
+        private TextView mTitleText;
+        private TextView mInfoText;
+        private ImageView mSportsImage;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+
+            mTitleText = itemView.findViewById(R.id.title);
+            mInfoText = itemView.findViewById(R.id.subTitle);
+            mSportsImage = itemView.findViewById(R.id.sportsImage);
+
+            itemView.setOnClickListener(this);
+        }
+
+        void bindTo(Sport currentSport){
+            mTitleText.setText(currentSport.getTitle());
+            mInfoText.setText(currentSport.getInfo());
+
+            Glide.with(mContext).load(
+                    currentSport.getImageResource()).into(mSportsImage);
+
+        }
+
+        @Override
+        public void onClick(View view) {
+            Sport currentSport = mSportsData.get(getAdapterPosition());
+            Intent detailIntent = new Intent(mContext, DetailActivity.class);
+            detailIntent.putExtra("title", currentSport.getTitle());
+            detailIntent.putExtra("image_resource",
+                    currentSport.getImageResource());
+            mContext.startActivity(detailIntent);
+        }
+    }
+}
